47f94c9c78dfe76988115ecc7e5e5754e4e2bb0c
maximo <maximopippo@gmail.com>
2025-10-10 03:19:58 -0300

    nada

--- vistaprevia/models.py
+++ vistaprevia/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
+class Producto(models.Model):
+    producto = models.CharField(max_length= 200)
+    fecha_de_publicacion = models.DateTimeField('Fecha de publicacion')
 
 #aca ponemos los ORM en principio, mas adelante vamos a tener nuestros propios mdoelos en una zona aparte
