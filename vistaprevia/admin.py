5342018655b457bdf96e22edcf3b88c544d2479e
maximo <maximopippo@gmail.com>
2025-10-10 08:33:26 -0300

    cambios del video

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin 
+
+from vistaprevia.models import Producto 
+
+ 
+
+admin.site.register(Producto) 
--- vistaprevia/models.py
+++ vistaprevia/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Producto(models.Model):
     producto = models.CharField(max_length= 200)
     fecha_de_publicacion = models.DateTimeField('Fecha de publicacion')
+    #este metodo str nos permite mostrar el nombre que le ponemos en el sitio admin de la pagina web
+    def __str__(self, ):
+        return self.producto + "---" + str(self.fecha_de_publicacion)
 
 #aca ponemos los ORM en principio, mas adelante vamos a tener nuestros propios mdoelos en una zona aparte
