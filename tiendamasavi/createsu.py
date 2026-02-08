bd09f942cc7c2556ad1fbfb687c06c928ce753f8
maximo <maximopippo@gmail.com>
2026-02-08 16:56:59 -0300

    fix createsu path for Render

--- tiendamasavi/createsu.py
+++ tiendamasavi/createsu.py
@@ -1,19 +1,20 @@
 import os
 import django
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tiendamasavi.settings")
+
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = "admin"
-email = "admin@masavi.com"
-password = "admin1234"  # después lo cambiás
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superusuario creado")
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@admin.com", "admin1234")
+    print("✔ Superusuario creado")
 else:
-    print("El superusuario ya existe")
+    print("ℹ Superusuario ya existe")
